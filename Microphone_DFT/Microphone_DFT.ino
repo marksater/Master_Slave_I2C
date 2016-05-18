65674489e7ed3954838713e39a959bca6fb0fcf0
Mark Sater <markjsater@gmail.com>
2016-05-18 16:23:15 -0600

    added anoter transmission to master and converted N decrelation into interger and everything that goes along with that...

--- Microphone_DFT/Microphone_DFT.ino
+++ Microphone_DFT/Microphone_DFT.ino
@@ -2,14 +2,14 @@
 
 const int button = 3;
 const int mic = A0;
+const int N = 600.0; //Number of samples
 const float pi = 3.141592654;
-const float N = 600.0; //Number of samples
 float T; // k is divided by delta times number of samples or divided by t (Time)
-float f[600];  //Signal array
+float f[N];  //Signal array
 float xSum, ySum, delta;
 signed long startTime, endTime, elTime;
-float Fx[300], Fy[300]; // Real and imaginary components of transform
-float Mag[300], Freg[300];
+float Fx[N/2], Fy[N/2]; // Real and imaginary components of transform
+float Mag[N/2], Freq[N/2];
 unsigned int k,n,r,i ;
 boolean dftProcess = false;
 boolean recording = false;
@@ -23,32 +23,23 @@ void setup()
 
 void loop() 
 {
+  //record the signal
   int buttonPush = digitalRead(button);
-  
   if(buttonPush == HIGH && dftProcess == false && recording == false){
     Record();
   }
-  Serial.println(buttonPush);
+
+  //run the transform
   if(dftProcess == true && recording == false){
-    byte Transmission=0;  
-    for(k=0; k <= N/2-1; k++){
-      xSum=0; ySum =0;
-      for(n=0;n<=N-1;n++){
-        float arg = 2*pi*k*n/N;
-        xSum = xSum + f[n]*cos(arg);
-        ySum = ySum + f[n]*sin(arg);
-      }
-      Fx[k]=(1.0/N)*xSum;
-      Fy[k]=(1.0/N)*(-ySum);
-      Mag[k]=sqrt( pow(Fx[k],2)+pow(Fy[k],2) );
-      Freg[k]=float(k)/T;// k/(T)or you can use k/(N*delta)
-    }
+    byte Transmission=0;
+    
+    Wire.beginTransmission(8);// begin transmission to uno
+    Wire.write("Wait");
+    DFT();   
     Transmission = GetMaxFrequency();
-    Wire.beginTransmission(8);
     Wire.write(Transmission);
     Wire.endTransmission();
     dftProcess = false;
-    Serial.print("end of dft");Serial.print("byte: ");Serial.println(sizeof(Transmission));
   }
 }
 
@@ -63,14 +54,13 @@ float GetMaxFrequency()
   
   for (int i = 0; i <=N-1;i++){
     if (Mag[i] == maxFreq){
-      return Freg[i];
+      return Freq[i];
     }
   }
 }
 
 void Record()
 {
-  
   recording = true;
   Microphone();
   recording = false;
@@ -90,14 +80,29 @@ void Microphone()
   }
   endTime = millis();
   elTime = endTime - startTime;
-  delta = (elTime / N)/1000.0;
+  delta = (elTime / float(N) )/1000.0;
   T = N*delta; // changed to N instead of N-1
-  Average=sum/N;
+  Average = sum / float(N);
   for(r=0;r<=N-1;r++){
     f[r]=500*(f[r]-Average);
     Serial.println(f[r]);
   }
-  Serial.println("================================================="); 
 }
 
+void DFT()
+{
+  
+  for(k = 0; k <= N/2-1; k++){
+    xSum = 0; ySum = 0;
+    for(n = 0; n <= N-1; n++){
+      float arg = 2*pi*k*n/N;
+      xSum = xSum+f[n]*cos(arg);
+      ySum = ySum+f[n]*sin(arg);
+    }
+    Fx[k] = (1.0/float(N))*xSum;
+    Fy[k] = (1.0/float(N))*(-ySum);
+    Mag[k] = sqrt( pow(Fx[k],2)+pow(Fy[k],2) );
+    Freq[k] = float(k)/T;// k/(T)or you can use k/(N*delta)
+  } 
+}
 
