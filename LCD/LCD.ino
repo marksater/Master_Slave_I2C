ad6e9956300d6e50c741e0c25c8fceb40bd8f0a4
Mark Sater <markjsater@gmail.com>
2016-05-18 18:00:26 -0600

    added functions and a charecter in recieve event funtions and cleaned up code

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -2,89 +2,118 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(4,6,10,11,12,13); 
 
+unsigned int frequencyTarget=200;
+unsigned int frequency;
+
+const int maxFrequency=300;
+const int minFrequency=32;
+const int increment = 1;
 const int toneButton = 3;
 const int buttonUp = 7;
 const int buttonDown = 8;
-const int increment = 1;
-int frequencyTarget=200;
-const int maxFrequency=399;
-const int minFrequency=2;
 const int DO = 2; //Pin for Digital Output - DO
 const int DA = A0; // Pin for Analog Output - AO
-int sensorvalue = 0;
+
 
 
 void setup()
 {
+  
   Wire.begin(8);
   lcd.begin (16,2);
-  pinMode(buttonUp,INPUT);
-  pinMode(buttonDown,INPUT);
+  
   lcd.setCursor (0,0);
   lcd.print ("Selector: ");
-  lcd.setCursor (10,0);
-  lcd.print (frequencyTarget);
-  
-
-}
-
-void loop() {
-  Serial.begin(9600);
   lcd.setCursor(0,1);
   lcd.print("Frequency: ");
-  lcd.setCursor(11,1);
-  Wire.onReceive(receiveEvent);
- 
-  delay(150);
   
-lcd.setCursor (10,0);
-if(digitalRead (buttonUp) == HIGH && digitalRead(buttonDown) == LOW )
-  {
-    //tone(9, frequencyTarget);
-    if(frequencyTarget <= maxFrequency)
-    {
-    frequencyUp();
-    }
-  }
- if(digitalRead (buttonDown) == HIGH && digitalRead(buttonUp) == LOW )
-  {
-    //tone(9, frequencyTarget);
-    if(frequencyTarget >= minFrequency)
-    {
-    frequencyDown();
-    }
-  }
- if(frequencyTarget < 100)
-  {
-    lcd.setCursor(12,0);
-    lcd.print(" ");
-  }
- if(digitalRead(toneButton) == HIGH)
-  {
-    tone(9, frequencyTarget);
-  }
- if(digitalRead(toneButton) == LOW)
-  {
-    noTone(9);
-  }
+  lcd.setCursor (10,0);
+  lcd.print (frequencyTarget);
 }
+
+void loop() 
+{
+  
+  Wire.onReceive(receiveEvent);
+  delay(150);
+  PushButton();
+  ScreenAdjustment();
+}
+
 void frequencyUp()
 {
-    frequencyTarget = frequencyTarget + increment;
-    lcd.print(frequencyTarget);  
-    delay(150);
+  
+  frequencyTarget = frequencyTarget + increment;
+  lcd.print(frequencyTarget);  
+  delay(150);
 }
 
 void frequencyDown()
 {
-    frequencyTarget = frequencyTarget - increment;
-    lcd.print(frequencyTarget);
-    delay(150);
+  
+  frequencyTarget = frequencyTarget - increment;
+  lcd.print(frequencyTarget);
+  delay(150);
 }
 
-void receiveEvent(int bytes){
-  int x = Wire.read();
-  lcd.print(x);
+void receiveEvent(int bytes)
+{
+  while( 1 < Wire.available()){
+    char c = Wire.read();
+    lcd.setCursor(11,1);
+    lcd.print(c); 
+  }
+  frequency = Wire.read();
+  lcd.setCursor(11,1);
+  lcd.print(frequency);
 }
- 
+
+void PushButton()
+{
+  lcd.setCursor (10,0);
+  if(digitalRead (buttonUp) == HIGH && digitalRead(buttonDown) == LOW ){
+    if(frequencyTarget < maxFrequency){
+      frequencyUp();
+    }
+  }
+  if(digitalRead (buttonDown) == HIGH && digitalRead(buttonUp) == LOW ){
+    if(frequencyTarget > minFrequency){
+      frequencyDown();
+    }
+  }
+  if(digitalRead(toneButton) == HIGH){
+    tone(9, frequencyTarget);
+  }
+  if(digitalRead(toneButton) == LOW){
+    noTone(9);
+  }
+}
+
+void ScreenAdjustment()
+{
+
+  //selector adjustment
+  if(frequencyTarget < 10){
+    lcd.setCursor(11,0);
+    lcd.print(" ");
+  }
+  if(frequencyTarget < 100){
+    lcd.setCursor(12,0);
+    lcd.print(" ");
+  }  
+
+  //frequency adjustment
+  if(frequency < 10){
+    lcd.setCursor(12,1);
+    lcd.print(" ");
+  }
+  if(frequency < 100){
+    lcd.setCursor(13,1);
+    lcd.print(" ");  
+  }
+  if(frequency < 1000){
+    lcd.setCursor(14,1);
+    lcd.print(" ");
+  }
+}
 
